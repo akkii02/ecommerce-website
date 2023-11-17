ff7a9aea2df591e67d13f44529f88cc455903106
akshaysable02 <akshaysable097@gmail.com>
2023-11-17 15:10:46 +0530

    new

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart = () => {
+  return (
+    <div className="d-flex align-items-center">
+      <h5 className="mb-0 text-light me-2">Cart</h5>
+      <span className="badge bg-primary">0</span>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube, faFacebook, faSpotify } from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+  return (
+    <div className="text-light py-5" style={{ backgroundColor: '#007bff' }}>
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs={12} md={6} className="text-center">
+            <div>
+                  <h1> The Generics</h1>
+                         </div>
+          </Col>
+          <Col xs={12} md={6} className="text-center">
+            <ul className="list-inline align-items-center" >
+              <li className="list-inline-item">
+                <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faYoutube} style={{ fontSize: '30px', color: 'white' }} />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://spotify.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faSpotify} style={{ fontSize: '30px', color: 'white' }} />
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faFacebook} style={{ fontSize: '30px', color: 'white' }} />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Footer;
